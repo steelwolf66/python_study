d2754472f7c6d167c1525a30ac4de275feb65b92
steelWolf <15144639564@163.com>
2025-03-27 16:49:14 +0800

    学习

---
+++ common_study/DefStudy.py
@@ -0,0 +1,7 @@
+def test(*args):
+    print(args)
+
+
+if __name__ == '__main__':
+    test('a', 'b', True)
+    pass
---
+++ common_study/ListComprehension.py
@@ -0,0 +1,4 @@
+# 列表推导
+squares = [x * 2 for x in range(10)]
+print(f'squares{squares}')
+
